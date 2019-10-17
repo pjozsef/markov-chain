357026e02bb84fd3d26a4a4e4f5cdc26acd515e3
pjozsef <9980358+pjozsef@users.noreply.github.com>
2019-10-18 00:47:42 +0200

    feat(chain): add cli interface

---
+++ markov-chain-cli/src/main/kotlin/com/github/pjozsef/markovchain/cli/Main.kt
@@ -0,0 +1,82 @@
+@file:JvmName("Main")
+
+package com.github.pjozsef.markovchain.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.*
+import com.github.ajalt.clikt.parameters.types.file
+import com.github.ajalt.clikt.parameters.types.int
+import com.github.pjozsef.markovchain.Constraints
+import com.github.pjozsef.markovchain.MarkovChain
+import com.github.pjozsef.markovchain.util.TransitionRule
+import com.github.pjozsef.markovchain.util.asDice
+import java.io.File
+import java.util.*
+
+class MainCommand : CliktCommand() {
+
+    companion object {
+        private const val RADIX_36 = 36
+    }
+
+    val words: File by argument().file(exists = true, folderOkay = false, readable = true)
+
+    val seed: String? by option("--seed")
+    val count: Int by option("--count").int().default(10)
+    val order: Int by option("--order").int().default(3)
+    val allowedRetries: Int by option("--retries").int().default(1000000)
+
+    val minLength: Int by option("--min").int().default(3)
+    val maxLength: Int by option("--max").int().default(8)
+    val startsWith: String? by option("--starts")
+    val endsWith: String? by option("--ends")
+    val contains: Collection<String>? by option("--contains").split(",")
+    val notContains: Collection<String>? by option("--not-contains").split(",")
+    val excluding: Collection<String>? by option("--excluding").split(",")
+    val hybridPrefixPostfix: Boolean by option("--useHybridPrefixPostfix").flag(default = true)
+
+    override fun run() {
+        println(words)
+
+        val random = initializeRandom()
+
+        val constraints = getConstraints()
+
+        val chain = MarkovChain(
+            TransitionRule.fromWords(
+                words.readLines(),
+                order = order
+            ).asDice(random),
+            allowedRetries = allowedRetries
+        )
+        chain.generate(
+            order = order,
+            count = count,
+            constraints = constraints
+        ).shuffled(random)
+            .take(count)
+            .toSortedSet()
+            .forEach(::println)
+    }
+
+    private fun initializeRandom(): Random {
+        val randomSeed = seed?.toLong(RADIX_36) ?: Random().nextLong()
+        println("Seed: ${randomSeed.toString(RADIX_36)}")
+        return Random(randomSeed)
+    }
+
+    private fun getConstraints() = Constraints(
+        minLength,
+        maxLength,
+        startsWith,
+        endsWith,
+        contains,
+        notContains,
+        excluding,
+        hybridPrefixPostfix
+    )
+
+}
+
+fun main(args: Array<String>) = MainCommand().main(args)
