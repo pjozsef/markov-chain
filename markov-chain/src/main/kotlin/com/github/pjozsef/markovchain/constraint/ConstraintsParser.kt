136fdd03b582db9f70bcf9026cf4ba9e15f24abb
pjozsef <9980358+pjozsef@users.noreply.github.com>
2020-03-25 22:08:51 +0100

    feat(constraint): add constraintParser

---
+++ markov-chain/src/main/kotlin/com/github/pjozsef/markovchain/constraint/ConstraintsParser.kt
@@ -0,0 +1,65 @@
+package com.github.pjozsef.markovchain.constraint
+
+import kotlin.reflect.KProperty0
+
+fun parseConstraint(stringConstraints: String) =
+    stringConstraints.split(",").map {
+        it.trim()
+    }.fold(Constraints()) { constraints, input ->
+        var newConstraints = constraints
+
+        lengthRegex.matchEntire(input)?.let {
+            it.groups["min"]?.value?.toInt()?.also {
+                newConstraints = newConstraints.copy(minLength = it)
+            }
+            it.groups["max"]?.value?.toInt()?.also {
+                newConstraints = newConstraints.copy(maxLength = it)
+            }
+        }
+
+        contentRegex.matchEntire(input)?.let {
+            it.groups["start"]?.value?.also {
+                if (it.startsWith("!")) {
+                    newConstraints = newConstraints.copy(notStartsWith = newList(it, newConstraints::notStartsWith))
+                } else {
+                    newConstraints = newConstraints.copy(startsWith = it)
+                }
+            }
+            it.groups["contains"]?.value?.also {
+                if (it.startsWith("!")) {
+                    newConstraints = newConstraints.copy(notContains = newList(it, newConstraints::notContains))
+                } else {
+                    newConstraints = newConstraints.copy(contains = newList(it, newConstraints::contains))
+                }
+            }
+            it.groups["end"]?.value?.also {
+                if (it.startsWith("!")) {
+                    newConstraints = newConstraints.copy(notEndsWith = newList(it, newConstraints::notEndsWith))
+                } else {
+                    newConstraints = newConstraints.copy(endsWith = it)
+                }
+            }
+        }
+
+        if (input == "!hybrid") {
+            newConstraints = newConstraints.copy(hybridPrefixPostfix = false)
+        }
+
+        if (input.contains("|")) {
+            input.split("|").map { it.trim() }.filter { it.isNotBlank() }.also {
+                val excludingList = newConstraints.excluding?.plus(it) ?: it
+                newConstraints = newConstraints.copy(excluding = excludingList)
+            }
+        }
+
+        newConstraints
+    }
+
+private val lengthRegex by lazy { Regex("(?<min>\\d)?-(?<max>\\d)?") }
+private val contentRegex by lazy { Regex("(?<start>[^*]+)?\\*(?<contains>[^*]+)?\\*(?<end>[^*]+)?") }
+
+private fun newList(value: String, property: KProperty0<Collection<String>?>): List<String> {
+    val element = if (value.startsWith("!")) value.drop(1) else value
+    val elementList = listOf(element)
+    return property.get()?.plus(elementList) ?: elementList
+}
---
+++ markov-chain/src/test/kotlin/com/github/pjozsef/markovchain/constraint/ConstraintsParserKtTest.kt
@@ -0,0 +1,121 @@
+package com.github.pjozsef.markovchain.constraint
+
+import io.kotlintest.IsolationMode
+import io.kotlintest.data.suspend.forall
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.FreeSpec
+import io.kotlintest.tables.row
+
+class ConstraintsParserKtTest : FreeSpec({
+    "parse" - {
+        forall(
+            row(
+                "min length",
+                "2-",
+                Constraints(minLength = 2)
+            ),
+            row(
+                "max length",
+                "-8",
+                Constraints(maxLength = 8)
+            ),
+            row(
+                "min-max length",
+                "2-8",
+                Constraints(minLength = 2, maxLength = 8)
+            ),
+            row(
+                "input trimmed",
+                "     2-8    ",
+                Constraints(minLength = 2, maxLength = 8)
+            ),
+            row(
+                "min-max as separate",
+                "2-, -8",
+                Constraints(minLength = 2, maxLength = 8)
+            ),
+            row(
+                "startsWith",
+                "prefix**",
+                Constraints(startsWith = "prefix")
+            ),
+            row(
+                "notStartsWith",
+                "!prefix**",
+                Constraints(notStartsWith = listOf("prefix"))
+            ),
+            row(
+                "multiple notStartsWith",
+                "!prefix**, !p2**, !a**",
+                Constraints(notStartsWith = listOf("prefix", "p2", "a"))
+            ),
+            row("endsWith", "**suffix", Constraints(endsWith = "suffix")),
+            row(
+                "notEndsWith",
+                "**!suffix",
+                Constraints(notEndsWith = listOf("suffix"))
+            ),
+            row(
+                "multiple notEndsWith",
+                "**!suffix, **!p2, **!a",
+                Constraints(notEndsWith = listOf("suffix", "p2", "a"))
+            ),
+            row(
+                "contains",
+                "*contains*",
+                Constraints(contains = listOf("contains"))
+            ),
+            row(
+                "multiple contains",
+                "*contains*, *a*, *b*",
+                Constraints(contains = listOf("contains", "a", "b"))
+            ),
+            row(
+                "notContains",
+                "*!contains*",
+                Constraints(notContains = listOf("contains"))
+            ),
+            row(
+                "multiple notContains",
+                "*!contains*, *!a*, *!b*",
+                Constraints(notContains = listOf("contains", "a", "b"))
+            ),
+            row(
+                "startsWith, contains, endsWith together",
+                "a*b*c",
+                Constraints(startsWith = "a", contains = listOf("b"), endsWith = "c")
+            ),
+            row(
+                "notStartsWith, notContains, notEndsWith together",
+                "!a*!b*!c",
+                Constraints(notStartsWith = listOf("a"), notContains = listOf("b"), notEndsWith = listOf("c"))
+            ),
+            row(
+                "no hybrid prefix postfix strategy",
+                "!hybrid",
+                Constraints(hybridPrefixPostfix = false)
+            ),
+            row(
+                "excluding words",
+                "ex1|ex2|ex3",
+                Constraints(excluding = listOf("ex1", "ex2", "ex3"))
+            ),
+            row(
+                "excluding words trimmed",
+                "ex1 | ex2  |    ex3|     ||",
+                Constraints(excluding = listOf("ex1", "ex2", "ex3"))
+            ),
+            row(
+                "multiple excluding",
+                "ex1|,  ex2|ex3|",
+                Constraints(excluding = listOf("ex1", "ex2", "ex3"))
+            )
+        ) { test, input, expected ->
+            test {
+                parseConstraint(input) shouldBe expected
+            }
+        }
+    }
+}) {
+    override fun isolationMode() = IsolationMode.InstancePerLeaf
+}
